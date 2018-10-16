9b2ca6906a098701246e5047b2e1babef7604d17
Joel Morales <joel.moralesmorales@hotmail.com>
2018-10-16 11:58:57 -0500

    Delete bin from master

--- bin/sql/data.sql
+++
@@ -1,3 +0,0 @@
-insert into AIRCRAFTCAPABILITIES  values (1, 'SeatCounts' ,'147' )
-insert into AIRCRAFTCAPABILITIES  values (2, 'SeatBealts' ,'150' )
-insert into AIRCRAFTCAPABILITIES  values (3, 'Wifi' ,'2' )
